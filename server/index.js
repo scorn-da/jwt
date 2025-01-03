0b1f15e937e9a6e10dee0f41fd12fbf6e3cb6d04
Dmitriy <varganovda@gmail.com>
2025-01-03 15:07:08 +0300

    feat(code, ui): added a prisma orm to work with the database

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
   try {
+    await ;
     app.listen(PORT, () => console.log(`Server's been started on the ${PORT} port`));
   } catch (e) {
     console.error(e);
